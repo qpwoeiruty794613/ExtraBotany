11112370f01a94ea72563b4c6caf3b71393af279
ExtraMeteorP <1579205460@qq.com>
2016-06-21 20:39:54 +0800

    增加成就
    增加成就

--- src/main/java/com/meteor/extrabotany/common/achievement/ModAchievement.java
+++ src/main/java/com/meteor/extrabotany/common/achievement/ModAchievement.java
@@ -1,12 +1,15 @@
 package com.meteor.extrabotany.common.achievement;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Achievement;
 import net.minecraftforge.common.AchievementPage;
 import vazkii.botania.common.achievement.AchievementTriggerer;
 import vazkii.botania.common.core.handler.ConfigHandler;
+import vazkii.botania.common.item.block.ItemBlockSpecialFlower;
 
 import com.meteor.extrabotany.common.item.ModItems;
 import com.meteor.extrabotany.common.lib.LibAchievementName;
+import com.meteor.extrabotany.common.lib.LibBlockName;
 import com.meteor.extrabotany.common.lib.LibItemName;
 import com.meteor.extrabotany.common.lib.LibReference;
 
@@ -33,13 +36,80 @@ public class ModAchievement {
 	public static Achievement Relic_olympusguard;
 	public static Achievement Relic_cthulhueye;
 	
+	public static Achievement Relic_hermes;
+	public static Achievement Relic_claymore;
+	public static Achievement Relic_combat;
+	public static Achievement Relic_slience;
+	public static Achievement Relic_loki;
+	public static Achievement Relic_angel;
+	public static Achievement Relic_ship;
+	
+	
 	//Gaia
 	public static Achievement Gaia_gaia3Kill;
 	public static Achievement Gaia_gaia3NoArmor;
 	public static Achievement Gaia_gaia3DarkKill;
 	public static Achievement Gaia_gaia3DarkNoArmor;
 	
+	//flower
+	public static Achievement F_annoy;
+	public static Achievement F_artifa;
+	public static Achievement F_diplop;
+	public static Achievement F_ice;
+	public static Achievement F_launch;
+	public static Achievement F_necro;
+	public static Achievement F_numeb;
+	public static Achievement F_numed;
+	public static Achievement F_void;
+	public static Achievement F_vola;
+	public static Achievement F_woo;
+	public static Achievement F_blue;
+	public static Achievement F_candy;
+	public static Achievement F_gemini;
+	public static Achievement F_moon;
+	public static Achievement F_omni;
+	public static Achievement F_pyscho;
+	public static Achievement F_stone;
+	public static Achievement F_sun;
+	public static Achievement F_infer;
+	public static Achievement F_judas;
+	public static Achievement F_mana;
+	
+	//others
+	public static Achievement O_blank;
+	
 	public static void init(){
+		
+		O_blank = new AchievementMod("blankcard", 1, 0, new ItemStack(ModItems.material, 1, 1), null);
+		
+		F_annoy = new AchievementMod(LibBlockName.ANNOYOBLOOM, 0, 1, ItemBlockSpecialFlower.ofType(LibBlockName.ANNOYOBLOOM), O_blank);
+		F_artifa = new AchievementMod(LibBlockName.ARTIFACONIA, 2, 1, ItemBlockSpecialFlower.ofType(LibBlockName.ARTIFACONIA), O_blank);
+		F_diplop = new AchievementMod(LibBlockName.DIPLOPBAMBOO, 0, 2, ItemBlockSpecialFlower.ofType(LibBlockName.DIPLOPBAMBOO), O_blank);
+		F_ice = new AchievementMod(LibBlockName.ICEBIRDIUM, 2, 2, ItemBlockSpecialFlower.ofType(LibBlockName.ICEBIRDIUM), O_blank);
+		F_launch = new AchievementMod(LibBlockName.LAUNCHISH, 0, 3, ItemBlockSpecialFlower.ofType(LibBlockName.LAUNCHISH), O_blank);
+		F_necro = new AchievementMod(LibBlockName.NECRO_FLUER, 2, 3, ItemBlockSpecialFlower.ofType(LibBlockName.NECRO_FLUER), O_blank);
+		F_numeb = new AchievementMod(LibBlockName.NUMERON_BALSAM, 0, 4, ItemBlockSpecialFlower.ofType(LibBlockName.NUMERON_BALSAM), O_blank);
+		F_numed = new AchievementMod(LibBlockName.NUMERON_DANDELIFE, 2, 4, ItemBlockSpecialFlower.ofType(LibBlockName.NUMERON_DANDELIFE), O_blank);
+		F_void = new AchievementMod(LibBlockName.VOIDUIM, 0, 5, ItemBlockSpecialFlower.ofType(LibBlockName.VOIDUIM), O_blank);
+		F_vola = new AchievementMod(LibBlockName.VOLATILILY, 2, 5, ItemBlockSpecialFlower.ofType(LibBlockName.VOLATILILY), O_blank);
+		F_woo = new AchievementMod(LibBlockName.WOODIENIA, 0, 6, ItemBlockSpecialFlower.ofType(LibBlockName.WOODIENIA), O_blank);
+		F_blue = new AchievementMod(LibBlockName.BLUE_ENCHANTRESS, 2, 6, ItemBlockSpecialFlower.ofType(LibBlockName.BLUE_ENCHANTRESS), O_blank);
+		F_candy = new AchievementMod(LibBlockName.CANDY_FLOWER, 0, 7, ItemBlockSpecialFlower.ofType(LibBlockName.CANDY_FLOWER), O_blank);
+		F_gemini = new AchievementMod(LibBlockName.GEMINIORCHID, 2, 7, ItemBlockSpecialFlower.ofType(LibBlockName.GEMINIORCHID), O_blank);
+		F_moon = new AchievementMod(LibBlockName.MOONLIGHT_LILY, 0, 8, ItemBlockSpecialFlower.ofType(LibBlockName.MOONLIGHT_LILY), O_blank);
+		F_omni = new AchievementMod(LibBlockName.OMNIVIOLET, 2, 8, ItemBlockSpecialFlower.ofType(LibBlockName.OMNIVIOLET), O_blank);
+		F_pyscho = new AchievementMod(LibBlockName.PYSCHOBLOOM, 0, 9, ItemBlockSpecialFlower.ofType(LibBlockName.PYSCHOBLOOM), O_blank);
+		F_stone = new AchievementMod(LibBlockName.STONESIA, 2, 9, ItemBlockSpecialFlower.ofType(LibBlockName.STONESIA), O_blank);
+		F_sun = new AchievementMod(LibBlockName.SUNSHINE_LILY, 0, 10, ItemBlockSpecialFlower.ofType(LibBlockName.SUNSHINE_LILY), O_blank);
+		F_infer = new AchievementMod(LibBlockName.INFERNOIDISY, 2, 10, ItemBlockSpecialFlower.ofType(LibBlockName.INFERNOIDISY), O_blank);
+		F_judas = new AchievementMod(LibBlockName.JUDASVOW, 0, 11, ItemBlockSpecialFlower.ofType(LibBlockName.JUDASVOW), O_blank);
+		F_mana = new AchievementMod(LibBlockName.MANALINKUIM, 2, 11, ItemBlockSpecialFlower.ofType(LibBlockName.MANALINKUIM), O_blank);
+		
+		Gaia_gaia3Kill = new AchievementMod(LibAchievementName.GAIA_KILL, -8, 8, vazkii.botania.common.item.ModItems.gaiaHead, null);
+		Gaia_gaia3NoArmor = new AchievementMod(LibAchievementName.GAIA_NOARMOR, -6, 8, ModItems.dungeonbox, null);
+		Gaia_gaia3DarkKill = new AchievementMod(LibAchievementName.GAIA_DARK, -8, 10, ModItems.dog, Gaia_gaia3Kill);
+		Gaia_gaia3DarkNoArmor = new AchievementMod(LibAchievementName.GAIA_DARKNOARMOR, -6, 10, ModItems.boxs, Gaia_gaia3DarkNoArmor);
+		
 		if(ConfigHandler.relicsEnabled) {
 			Relic_aphroditegrace = new AchievementMod(LibItemName.APHRODITEGRACE, -9, 12, ModItems.aphroditegrace, Gaia_gaia3Kill);
 			Relic_dice20 = new AchievementMod(LibItemName.DICE20, -7, 12, ModItems.dice20, Gaia_gaia3Kill);
@@ -56,14 +126,16 @@ public class ModAchievement {
 			Relic_phoenixblaster = new AchievementMod(LibItemName.PHOENIXBLASTER, -9, 16, ModItems.gunphoenixblaster, Gaia_gaia3Kill);
 			Relic_snowballcannon = new AchievementMod(LibItemName.SNOWBALLLAUNCHER, -7, 16, ModItems.snowballlauncher, Gaia_gaia3Kill);
 			Relic_tacticalshotgun = new AchievementMod(LibItemName.TACTICALSHOTGUN, -5, 16, ModItems.guntacticalshotgun, Gaia_gaia3Kill);
-			Relic_olympusguard = new AchievementMod(LibItemName.OLYMPUSGUARD, -9, 17, ModItems.olympusguard, Gaia_gaia3Kill);
-			Relic_cthulhueye = new AchievementMod(LibItemName.CTHULHUEYE, -7, 17, ModItems.cthulhueye, Gaia_gaia3Kill);		
+			Relic_olympusguard = new AchievementMod(LibItemName.OLYMPUSGUARD, -5, 17, ModItems.olympusguard, Gaia_gaia3Kill);
+			Relic_cthulhueye = new AchievementMod(LibItemName.CTHULHUEYE, -7, 17, ModItems.cthulhueye, Gaia_gaia3Kill);	
+			Relic_hermes = new AchievementMod(LibItemName.HERMESWAND, -9, 17, ModItems.hermeswand, Gaia_gaia3Kill);		
+			Relic_claymore = new AchievementMod(LibItemName.HELIACALCLAYMORE, -5, 18, ModItems.heliacalclaymore, Gaia_gaia3Kill);		
+			Relic_combat = new AchievementMod(LibItemName.VALKYRIECOMBATUNIFORM, -7, 18, ModItems.valkyriecombatuniform, Gaia_gaia3Kill);		
+			Relic_ship = new AchievementMod(LibItemName.THESEUSSHIP, -9, 18, ModItems.theseusship, Gaia_gaia3Kill);		
+			Relic_angel = new AchievementMod(LibItemName.ANGELWAND, -5, 19, ModItems.angelwand, Gaia_gaia3Kill);		
+			Relic_loki = new AchievementMod(LibItemName.LOKIGHOSTRICK, -7, 19, ModItems.lokighostrick, Gaia_gaia3Kill);		
+			Relic_slience = new AchievementMod(LibItemName.ETERNALSLIENCE, -9, 19, ModItems.eternalslience, Gaia_gaia3Kill);			
 		}
-		
-		Gaia_gaia3Kill = new AchievementMod(LibAchievementName.GAIA_KILL, -8, 8, vazkii.botania.common.item.ModItems.gaiaHead, null);
-		Gaia_gaia3NoArmor = new AchievementMod(LibAchievementName.GAIA_NOARMOR, -6, 8, ModItems.dungeonbox, null);
-		Gaia_gaia3DarkKill = new AchievementMod(LibAchievementName.GAIA_DARK, -8, 10, ModItems.dog, null);
-		Gaia_gaia3DarkNoArmor = new AchievementMod(LibAchievementName.GAIA_DARKNOARMOR, -6, 10, ModItems.boxs, null);
 		
 		int pageIndex = AchievementPage.getAchievementPages().size();
 		AchievementPage extrabotanyPage = new AchievementPage(LibReference.MOD_NAME, AchievementMod.achievements.toArray(new Achievement[AchievementMod.achievements.size()]));
--- src/main/java/com/meteor/extrabotany/common/event/EventAchievement.java
+++ src/main/java/com/meteor/extrabotany/common/event/EventAchievement.java
@@ -3,17 +3,71 @@ package com.meteor.extrabotany.common.event;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import vazkii.botania.common.item.block.ItemBlockSpecialFlower;
+
+import com.meteor.extrabotany.common.achievement.ModAchievement;
+import com.meteor.extrabotany.common.item.ModItems;
+import com.meteor.extrabotany.common.lib.LibBlockName;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
 
-public class EventAchievement {
+public class EventAchievement{
 	@SubscribeEvent
 	public void onItemPickedUp(ItemPickupEvent event) {
 		EntityItem e = event.pickedUp;
 		ItemStack stack = event.pickedUp.getEntityItem();
 		EntityPlayer player = (EntityPlayer) event.player;
         if(stack != null){
-        		
+        	if(stack.getItem() == ModItems.material && stack.getItemDamage() == 1)
+        		player.addStat(ModAchievement.O_blank, 1);
+        	if(stack.getItem() instanceof ItemBlockSpecialFlower){
+				String type = ItemBlockSpecialFlower.getType(stack);
+				if(type.equals(LibBlockName.ANNOYOBLOOM))
+						event.player.addStat(ModAchievement.F_annoy, 1);
+				else if(type.equals(LibBlockName.ARTIFACONIA))
+			    		event.player.addStat(ModAchievement.F_artifa, 1);
+				else if(type.equals(LibBlockName.DIPLOPBAMBOO))
+			   			event.player.addStat(ModAchievement.F_diplop, 1);
+				else if(type.equals(LibBlockName.ICEBIRDIUM))
+		    			event.player.addStat(ModAchievement.F_ice, 1);
+				else if(type.equals(LibBlockName.LAUNCHISH))
+			    		event.player.addStat(ModAchievement.F_launch, 1);
+				else if(type.equals(LibBlockName.NECRO_FLUER))
+			    		event.player.addStat(ModAchievement.F_necro, 1);
+				else if(type.equals(LibBlockName.NUMERON_BALSAM))
+			    		event.player.addStat(ModAchievement.F_numeb, 1);
+				else if(type.equals(LibBlockName.NUMERON_DANDELIFE))
+			    		event.player.addStat(ModAchievement.F_numed, 1);
+				else if(type.equals(LibBlockName.VOIDUIM))
+			    		event.player.addStat(ModAchievement.F_void, 1);
+				else if(type.equals(LibBlockName.VOLATILILY))
+			    		event.player.addStat(ModAchievement.F_vola, 1);
+				else if(type.equals(LibBlockName.WOODIENIA))
+			    		event.player.addStat(ModAchievement.F_woo, 1);
+				else if(type.equals(LibBlockName.BLUE_ENCHANTRESS))
+			    		event.player.addStat(ModAchievement.F_blue, 1);
+				else if(type.equals(LibBlockName.CANDY_FLOWER))
+			    		event.player.addStat(ModAchievement.F_candy, 1);
+				else if(type.equals(LibBlockName.GEMINIORCHID))
+			    		event.player.addStat(ModAchievement.F_gemini, 1);
+				else if(type.equals(LibBlockName.MOONLIGHT_LILY))
+			    		event.player.addStat(ModAchievement.F_moon, 1);
+				else if(type.equals(LibBlockName.OMNIVIOLET))
+			    		event.player.addStat(ModAchievement.F_omni, 1);
+				else if(type.equals(LibBlockName.PYSCHOBLOOM))
+			    		event.player.addStat(ModAchievement.F_pyscho, 1);
+				else if(type.equals(LibBlockName.STONESIA))
+			   			event.player.addStat(ModAchievement.F_stone, 1);
+				else if(type.equals(LibBlockName.SUNSHINE_LILY))
+			    		event.player.addStat(ModAchievement.F_sun, 1);
+				else if(type.equals(LibBlockName.INFERNOIDISY))
+			    		event.player.addStat(ModAchievement.F_infer, 1);
+				else if(type.equals(LibBlockName.JUDASVOW))
+			    		event.player.addStat(ModAchievement.F_judas, 1);
+				else if(type.equals(LibBlockName.MANALINKUIM))
+			    		event.player.addStat(ModAchievement.F_mana, 1);
+			}
 	    }
     }
 }
